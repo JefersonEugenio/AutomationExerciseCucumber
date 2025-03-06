9208d87bbf64434d633788031899faab9a43dfcc
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-03-06 15:05:13 -0300

    Adicionei criar inicia e finalizar reports

--- src/main/java/framework/supports/Hooks.java
+++ src/main/java/framework/supports/Hooks.java
@@ -1,22 +1,30 @@
 package framework.supports;
 
+import com.aventstack.extentreports.Status;
+import framework.tools.Report;
 import framework.webDrivers.DriverManager;
 import framework.webDrivers.Drivers;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
+import static framework.tools.Report.extentTest;
 import static framework.webDrivers.DriverFactory.getBrowser;
 
 public class Hooks {
 
     @Before
     public void setUp() {
+        Report.configurarExtentReport();
+        Report.createTest("Automation Exercise com Cucumber");
+        extentTest.log(Status.INFO, "Iniciado de teste");
         DriverManager.setDriver(getBrowser(Drivers.CHROME));
         DriverManager.getDriver().get("https://automationexercise.com/login");
     }
 
     @After
     public void tearDown() {
+        extentTest.log(Status.INFO, "Finalizado de teste");
+        Report.closeReport();
         DriverManager.quitDriver();
     }
 }
