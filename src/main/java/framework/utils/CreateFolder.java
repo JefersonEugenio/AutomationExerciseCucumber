ff58cef191b6e7c889a4e98bafc64c0fc81a08a5
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-03-12 23:12:16 -0300

    Adicionei um metodo para pagar pasta e arquivo

--- src/main/java/framework/utils/CreateFolder.java
+++ src/main/java/framework/utils/CreateFolder.java
@@ -1,8 +1,26 @@
 package framework.utils;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
 
 public class CreateFolder {
+
+    private static final String CAMINHO_RESOURCES = System.getProperty("user.dir") + File.separator +
+            "src" + File.separator + "main" + File.separator + "resources" + File.separator + "properties" + File.separator;
+
+    public static void deleteDirectoryRecursively(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    deleteDirectoryRecursively(entry);
+                }
+            }
+        }
+        Files.delete(file.toPath());
+    }
 
     public static void createDirectory(String path){
         File directory = new File(path);
