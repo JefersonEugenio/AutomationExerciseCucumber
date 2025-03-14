e3b78fcd1f4958e914c6de0995cf01cedf68f655
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-03-14 20:21:36 -0300

    Modifiquei os codigos arquivo de propriedade vazio para não fica erro na pipeline

--- src/main/java/framework/utils/ProdutoProperties.java
+++ src/main/java/framework/utils/ProdutoProperties.java
@@ -5,24 +5,31 @@ import java.util.Properties;
 
 public class ProdutoProperties {
 
-    private static final String CAMINHO_RESOURCES = System.getProperty("user.dir") + File.separator +
+    private static final String CAMINHO_ARQUIVO = System.getProperty("user.dir") + File.separator +
             "src" + File.separator + "main" + File.separator + "resources" + File.separator + "properties"
             + File.separator + "produto.properties";
 
     public static void salvarProduto(String chave, String valor) {
         Properties prop = new Properties();
+        File arquivo = new File(CAMINHO_ARQUIVO);
 
-        try (FileInputStream in = new FileInputStream(CAMINHO_RESOURCES)) {
-            prop.load(in);
-        } catch (FileNotFoundException e) {
-            // Se o arquivo ainda não existe, não há problema (será criado depois)
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (!arquivo.exists()) {
+            try {
+                arquivo.createNewFile();
+            } catch (IOException e) {
+                System.err.println("Erro ao criar o arquivo de propriedades: " + e.getMessage());
+                return;
+            }
         }
 
+        try (FileInputStream in = new FileInputStream(arquivo)) {
+            prop.load(in);
+        } catch (IOException e) {
+            System.err.println("Aviso: Não foi possível carregar o arquivo de propriedades. Pode estar vazio ou não existir.");
+        }
         prop.setProperty(chave, valor);
 
-        try (FileOutputStream out = new FileOutputStream(CAMINHO_RESOURCES)) {
+        try (FileOutputStream out = new FileOutputStream(CAMINHO_ARQUIVO)) {
             prop.store(out, "Dados do Produto");
         } catch (IOException e) {
             e.printStackTrace();
@@ -31,7 +38,7 @@ public class ProdutoProperties {
 
     public static String obterProdutoRetorno(String chave) {
         Properties prop = new Properties();
-        try (FileInputStream in = new FileInputStream(CAMINHO_RESOURCES)) {
+        try (FileInputStream in = new FileInputStream(CAMINHO_ARQUIVO)) {
             prop.load(in);
             return prop.getProperty(chave);
         } catch (IOException e) {
