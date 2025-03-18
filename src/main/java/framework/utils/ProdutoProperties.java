c7f9a7393aaab07191d9283ba17ba5c820de599d
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-03-18 11:14:09 -0300

    Adicionei novamente codigo properties

--- src/main/java/framework/utils/ProdutoProperties.java
+++ src/main/java/framework/utils/ProdutoProperties.java
@@ -6,16 +6,39 @@ import java.util.Properties;
 public class ProdutoProperties {
 
     private static final String CAMINHO_ARQUIVO = System.getProperty("user.dir") + File.separator +
-            "src" + File.separator + "main" + File.separator + "resources" + File.separator + "properties"
-            + File.separator + "Produtos.txt";
+            "src" + File.separator + "main" + File.separator + "resources" + File.separator + "Produtos.properties";
 
-    public static void salvarProduto(String valor) {
-        try (FileWriter writer = new FileWriter(CAMINHO_ARQUIVO, true)) {
-            writer.write(valor + System.lineSeparator());
+    public static void salvarProduto(String chave, String valor) {
+        Properties prop = new Properties();
+        File arquivo = new File(CAMINHO_ARQUIVO);
+
+        if (!arquivo.exists()) {
+            try {
+                if (arquivo.createNewFile()) {
+                    System.out.println("Arquivo criado: " + arquivo.getAbsolutePath());
+                }
+            } catch (IOException e) {
+                System.err.println("Erro ao criar o arquivo de propriedades: " + e.getMessage());
+                return;
+            }
+        }
+
+        try (FileInputStream in = new FileInputStream(arquivo)) {
+            prop.load(in);
+        } catch (FileNotFoundException e) {
+            // Se o arquivo ainda não existe, não há problema (será criado depois)
         } catch (IOException e) {
-            System.out.println("Erro ao criar/escrever no arquivo.");
+            System.err.println("Aviso: Não foi possível carregar o arquivo de propriedades. Pode estar vazio ou não existir.");
+        }
+
+        prop.setProperty(chave, valor);
+
+        try (FileOutputStream out = new FileOutputStream(CAMINHO_ARQUIVO)) {
+            prop.store(out, "Dados do Produto");
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 
     public static String obterProdutoRetorno(String chave) {
