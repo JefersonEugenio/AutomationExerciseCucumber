e106c254bc0ae9e009f143402a6a075bd6be4f5e
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-03-18 15:12:47 -0300

    Voltar que fiz metodo criar e apagar pasta

--- src/main/java/framework/utils/CreateFolder.java
+++ src/main/java/framework/utils/CreateFolder.java
@@ -8,6 +8,18 @@ import java.nio.file.Paths;
 
 public class CreateFolder {
 
+    public static void deleteDirectoryRecursively(File file) throws IOException {
+        if (file.isDirectory()) {
+            File[] entries = file.listFiles();
+            if (entries != null) {
+                for (File entry : entries) {
+                    deleteDirectoryRecursively(entry);
+                }
+            }
+        }
+        Files.delete(file.toPath());
+    }
+
     private static final String BASE_DIR = "src";
 
     private static Path buildPath(String directory, String folderName) {
@@ -15,12 +27,25 @@ public class CreateFolder {
     }
 
     public static void createFolder(String directory, String folderName) {
+
         Path folderPath = buildPath(directory, folderName);
 
         try {
             Files.createDirectories(folderPath);
         } catch (IOException e) {
             System.out.println("Erro ao criar a pasta '" + folderPath + "': " + e.getMessage());
+        }
+    }
+
+
+
+    public static void createDirectory(String path) throws IOException {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            directory.mkdir();
+        } else {
+            deleteDirectoryRecursively(new File(path));
+            createDirectory(path);
         }
     }
 
