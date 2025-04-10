c73876e5576b2ece25b2d7f1ec545cbf94ea9c72
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-04-10 16:39:50 -0300

    Desenvolvi os métodos para a tarefa

--- src/test/java/steps/login/LoginAutomationExercise.java
+++ src/test/java/steps/login/LoginAutomationExercise.java
@@ -1,49 +1,135 @@
-//package steps.login;
-//
-//import com.aventstack.extentreports.Status;
-//import framework.tools.Screenshot;
-//import framework.webDrivers.DriverManager;
-//import io.cucumber.java.pt.Dado;
-//import io.cucumber.java.pt.E;
-//import io.cucumber.java.pt.Então;
-//import io.cucumber.java.pt.Quando;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//
-//import static framework.tools.Report.extentTest;
-//
-//public class LoginAutomationExercise {
-//
-//    WebDriver driver = DriverManager.getDriver();
-//
-//    @Dado("que o usuario esta na pagina de login do sistema Automation Exercise")
-//    public void queOUsuarioEstaNaPaginaDeLoginDoSistemaAutomationExercise() {
-//        extentTest.log(Status.INFO, "A tela está página de login do sistema Automation Exercise");
-//    }
-//
-//    @Quando("o usuario preenche o campo email com {string}")
-//    public void oUsuarioPreencheOCampoEmailCom(String valor) {
-//        driver.findElement(By.name("email")).sendKeys(valor);
-//    }
-//
-//    @E("o usuario preenche o campo password com {string}")
-//    public void oUsuarioPreencheOCampoPasswordCom(String valor) {
-//        driver.findElement(By.name("password")).sendKeys(valor);
-//    }
-//
-//    @E("o usuario clica no botao login")
-//    public void oUsuarioClicaNoBotaoLogin() {
-//        driver.findElement(By.cssSelector(".login-form .btn-default")).click();
-//    }
-//
-//    @Então("o sistema autentica o usuario")
-//    public void oSistemaAutenticaOUsuario() throws InterruptedException {
-//        Thread.sleep(2000);
-//    }
-//
-//    @E("redireciona para a tela principal")
-//    public void redirecionaParaATelaPrincipal() throws IllegalAccessException {
-//        extentTest.log(Status.INFO, "Está acessando a tela de principal", Screenshot.screenshotBase64(driver));
-//    }
-//
-//}
+package steps.login;
+
+import com.aventstack.extentreports.Status;
+import framework.tools.Screenshot;
+import framework.webDrivers.DriverManager;
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.E;
+import io.cucumber.java.pt.Então;
+import io.cucumber.java.pt.Quando;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+import static framework.tools.Report.extentTest;
+
+public class LoginAutomationExercise {
+
+    WebDriver driver = DriverManager.getDriver();
+
+    @Dado("que o usuario esta na pagina de login do sistema Automation Exercise")
+    public void queOUsuarioEstaNaPaginaDeLoginDoSistemaAutomationExercise() {
+        extentTest.log(Status.INFO, "A tela está página de login do sistema Automation Exercise");
+    }
+
+    @Quando("localizar a seção Login to your account e o usuario preenche o campo email com {string}")
+    public void localizarASeçãoLoginToYourAccountEOUsuarioPreencheOCampoEmailCom(String valor) {
+        List<WebElement> emails = driver.findElements(By.cssSelector("#form .container .row input[name=email]"));
+        emails.get(0).sendKeys(valor);
+        extentTest.log(Status.INFO, "O usuario preencheu o campo email");
+    }
+
+    @E("localizar a seção New User Signup e o usuario preenche o campo email com {string}")
+    public void localizarASeçãoNewUserSignupEOUsuarioPreencheOCampoEmailCom(String valor) {
+        List<WebElement> emails = driver.findElements(By.cssSelector("#form .container .row input[name=email]"));
+        emails.get(1).sendKeys(valor);
+        extentTest.log(Status.INFO, "O usuario preencheu o campo email");
+    }
+
+    @E("o usuario preenche o campo password com {string}")
+    public void oUsuarioPreencheOCampoPasswordCom(String valor) {
+        driver.findElement(By.name("password")).sendKeys(valor);
+        extentTest.log(Status.INFO, "O usuario preencheu o campo senha");
+    }
+
+    @E("o usuario clica no botao login")
+    public void oUsuarioClicaNoBotaoLogin() {
+        driver.findElement(By.cssSelector(".login-form .btn-default")).click();
+        extentTest.log(Status.INFO, "O usuario clicou no botão login");
+    }
+
+    @Então("o sistema autentica o usuario")
+    public void oSistemaAutenticaOUsuario() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    @E("redireciona para a tela principal")
+    public void redirecionaParaATelaPrincipal() throws IllegalAccessException {
+        extentTest.log(Status.INFO, "Está acessando a tela de principal", Screenshot.screenshotBase64(driver));
+    }
+
+    @Quando("o usuario não preenche o campo email")
+    public void oUsuarioNãoPreencheOCampoEmail() {
+        extentTest.log(Status.INFO, "O usuario não preenche o campo email" );
+    }
+
+    @E("o usuario não preenche o campo password")
+    public void oUsuarioNãoPreencheOCampoPassword() {
+        extentTest.log(Status.INFO, "O usuario não preenche o campo password" );
+    }
+
+    @Então("o sistema exibe a mensagem Preenche este campo")
+    public void oSistemaExibeAMensagemPreencheEsteCampo() throws IllegalAccessException {
+        extentTest.log(Status.PASS, "O sistema exibir mensagem no campo que 'Preenche este campo.'", Screenshot.screenshotBase64(driver));
+    }
+
+    @Então("o sistema exibe a mensagem {string}")
+    public void oSistemaExibeAMensagem(String mensagem) throws IllegalAccessException {
+        String mensagemWeb = driver.findElement(By.cssSelector("#form b")).getText();
+        Assertions.assertEquals(mensagem, mensagemWeb);
+        extentTest.log(Status.PASS, "Sua compra foi realizada com sucesso", Screenshot.screenshotBase64(driver));
+    }
+
+    @Quando("o usuario preenche o campo nome com {string}")
+    public void oUsuarioPreencheOCampoNomeCom(String valor) {
+        driver.findElement(By.name("name")).sendKeys(valor);
+        extentTest.log(Status.INFO, "O usuario preencheu o campo nome");
+    }
+
+    @Então("o sistema login exibe a mensagem {string}")
+    public void oSistemaLoginExibeAMensagem(String valor) throws IllegalAccessException, InterruptedException {
+        if (valor.contains("Your")) {
+            String mensagemWeb = driver.findElement(By.cssSelector(".login-form form p")).getText();
+            Assertions.assertEquals(valor, mensagemWeb);
+            extentTest.log(Status.PASS, "O sistema exibir mensagem no campo que 'Your email or password is incorrect!'", Screenshot.screenshotBase64(driver));
+        } else {
+            String mensagemWeb = driver.findElement(By.cssSelector(".signup-form form p")).getText();
+            Assertions.assertEquals(valor, mensagemWeb);
+            extentTest.log(Status.PASS, "O sistema exibir mensagem no campo que 'Email Address already exist!!'", Screenshot.screenshotBase64(driver));
+        }
+    }
+
+    @E("o usuario clica no botao signup")
+    public void oUsuarioClicaNoBotaoSignup() {
+        driver.findElement(By.cssSelector(".signup-form .btn-default")).click();
+        extentTest.log(Status.INFO, "O usuario clicou no botão signup");
+    }
+
+    @E("localizar a seção New User Signup e o usuario não preenche o campo email")
+    public void localizarASeçãoNewUserSignupEOUsuarioNãoPreencheOCampoEmail() {
+        extentTest.log(Status.INFO, "O usuario não preenche o campo email");
+
+    }
+
+    @Quando("o usuario não preenche o campo nome")
+    public void oUsuarioNãoPreencheOCampoNome() {
+        extentTest.log(Status.INFO, "O usuario não preenche o campo nome");
+    }
+
+    @Então("redireciona para a tela cadastrar")
+    public void redirecionaParaATelaCadastrar() throws IllegalAccessException {
+        List<WebElement> elementos = driver.findElements(By.cssSelector(".login-form .title b"));
+        for (WebElement elemento : elementos) {
+            if (elemento.getText().equals("ENTER ACCOUNT INFORMATION")) {
+                extentTest.log(Status.PASS, "A tela da página está na parte de cadastro", Screenshot.screenshotBase64(driver));
+                break;
+            } else {
+                extentTest.log(Status.FAIL, "A tela da página não está na parte de cadastro", Screenshot.screenshotBase64(driver));
+                break;
+            }
+        }
+    }
+}
