ba0fe845f2c03264781037d1b3d0ef02ca8e6364
Jeferson Lopes Eugenio <jefersonlopeseugenio11@gmail.com>
2025-04-15 16:07:28 -0300

    Modifiquei caminho para executar

--- src/test/java/runner/AutomationExerciseRunTest.java
+++ src/test/java/runner/AutomationExerciseRunTest.java
@@ -6,8 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/login",
-        glue = {"framework.supports", "steps.login"},
+        features = "src/test/resources/features/cadastrar",
+        glue = {"framework.supports", "steps"},
+        name = "Cadastras com validas",
         plugin = {"pretty", "html:target/HtmlReports.html"}
 )
 public class AutomationExerciseRunTest {
